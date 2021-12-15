7167075742a4022a1b437938470b79beb6a8983e
francisco_aguiar <francisco_aguiar99@hotmail.com>
2021-12-14 23:25:08 -0400

    criando janela

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Óla Mundo')
+from tkinter import *
+import tkinter.messagebox
+
+#cores-------------------
+cor0 = '#444466'  # preto
+cor1 = '#feffff'  # branco
+cor2 = '#004338'
+
+#--criando a janela -------------------
+
+janela = Tk()
+janela.geometry('530x205')
+janela.configure(bg=cor1)
+
+
+janela.mainloop()

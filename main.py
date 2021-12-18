839b496759107f3671a7c746755c08fde46e47b4
francisco_aguiar <francisco_aguiar99@hotmail.com>
2021-12-18 19:24:31 -0400

    configurando frame direita

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ janela.configure(bg=cor1)
 tela = Label(janela, bg=cor0, width=40, height=10, bd=1)
 tela.grid(row=0, column=0)
 
-frame_direita = Frame(janela, bg='red')
-frame_direita.grid(row=0, column=1)
+frame_direita = Frame(janela, bg=cor1)
+frame_direita.grid(row=0, column=1, padx=5)
 
 frame_baixo = Frame(janela, bg='red')
 frame_baixo.grid(row=1, column=0, columnspan=2, pady=20)
@@ -27,8 +27,18 @@ frame_baixo.grid(row=1, column=0, columnspan=2, pady=20)
 
 l_red = Label(frame_direita, text='Red', width=7, bg=cor1, fg='red', anchor='nw', font=('Time New Roman', 12, 'bold'))
 l_red.grid(row=0, column=0)
-s_red=Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='red', orient=HORIZONTAL)
-s_red.grid(row=0, column=0)
+s_red = Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='red', orient=HORIZONTAL)
+s_red.grid(row=0, column=1)
+
+l_green = Label(frame_direita, text='Green', width=7, bg=cor1, fg='green', anchor='nw', font=('Time New Roman', 12, 'bold'))
+l_green.grid(row=1, column=0)
+s_green = Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='green', orient=HORIZONTAL)
+s_green.grid(row=1, column=1)
+
+l_blue = Label(frame_direita, text='Blue', width=7, bg=cor1, fg='blue', anchor='nw', font=('Time New Roman', 12, 'bold'))
+l_blue.grid(row=2, column=0)
+s_blue = Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='blue', orient=HORIZONTAL)
+s_blue.grid(row=2, column=1)
 
 
 janela.mainloop()

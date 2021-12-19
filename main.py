2f7b34ef96e6a55f2b113782a54386adb2eeef03
francisco_aguiar <francisco_aguiar99@hotmail.com>
2021-12-18 21:49:21 -0400

    configurando as funções de escala de cores

--- main.py
+++ main.py
@@ -23,25 +23,48 @@ frame_direita.grid(row=0, column=1, padx=5)
 frame_baixo = Frame(janela, bg=cor1)
 frame_baixo.grid(row=1, column=0, columnspan=2, pady=20)
 
-# -- configurando  o frame direita ---------------
+# funções scale
+
+
+def escala(valor):
+    r = s_red.get()
+    g = s_green.get()
+    b = s_blue.get()
+
+    rgb = f'{r}, {g}, {b}'
+
+    hexadecimal = "#%02x%02x%02x" % (r, g, b)
+    tela['bg'] = hexadecimal
+
+
+# função clicar
+
+def onClick():
+    clip = Tk()
+    clip.withdraw()
+    clip.clipboard_append(e_cor.get())
+    clip.destroy()
+    print('e Nos')
+
+#  -- configurando  o frame direita ---------------
 
 l_red = Label(frame_direita, text='Red', width=7, bg=cor1, fg='red', anchor='nw', font=('Time New Roman', 12, 'bold'))
 l_red.grid(row=0, column=0)
-s_red = Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='red', orient=HORIZONTAL)
+s_red = Scale(frame_direita, command=escala, from_=0, to=255, length=150, bg=cor1, fg='red', orient=HORIZONTAL)
 s_red.grid(row=0, column=1)
 
-l_green = Label(frame_direita, text='Green', width=7, bg=cor1, fg='green', anchor='nw', font=('Time New Roman', 12, 'bold'))
+l_green = Label(frame_direita,  text='Green', width=7, bg=cor1, fg='green', anchor='nw', font=('Time New Roman', 12, 'bold'))
 l_green.grid(row=1, column=0)
-s_green = Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='green', orient=HORIZONTAL)
+s_green = Scale(frame_direita, command=escala, from_=0, to=255, length=150, bg=cor1, fg='green', orient=HORIZONTAL)
 s_green.grid(row=1, column=1)
 
 l_blue = Label(frame_direita, text='Blue', width=7, bg=cor1, fg='blue', anchor='nw', font=('Time New Roman', 12, 'bold'))
 l_blue.grid(row=2, column=0)
-s_blue = Scale(frame_direita, from_=0, to=255, length=150, bg=cor1, fg='blue', orient=HORIZONTAL)
+s_blue = Scale(frame_direita, command=escala, from_=0, to=255, length=150, bg=cor1, fg='blue', orient=HORIZONTAL)
 s_blue.grid(row=2, column=1)
 
 # -- configurando  o frame Baixo ---------------
-l_rgb = Label(frame_baixo, text='CÒDIGO RGB:', bg=cor1, font=('Ivy', 10, 'bold'))
+l_rgb = Label(frame_baixo, text='CÒDIGO HEX:', bg=cor1, font=('Ivy', 10, 'bold'))
 l_rgb.grid(row=0, column=0, padx=5)
 
 #Entry
@@ -49,7 +72,7 @@ e_cor = Entry(frame_baixo, width=15, font=("Ivy", 10, "bold"), justify=CENTER)
 e_cor.grid(row=0, column=1, padx=10)
 
 # Button copy
-b_copy = Button(frame_baixo, text='copy', bg=cor1, font=('Ivy', 10, 'bold'), relief=RAISED, overrelief=RIDGE)
+b_copy = Button(frame_baixo, command=onClick, text='copy', bg=cor1, font=('Ivy', 10, 'bold'), relief=RAISED, overrelief=RIDGE)
 b_copy.grid(row=0, column=2, padx=5)
 
 #app name
